f093791af2eb217a7601df9b86fe05bfb0e97c10
Renato Belini <renatobelini@gmail.com>
2024-04-11 16:37:20 -0300

    redimensionando imagem

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 def index(request):
     return render(
         request,
-        'blog/pages/index.html'
+        'blog/pages/index.html',
+        {
+            'nome' : 'renato belini'
+         }
     )
 
