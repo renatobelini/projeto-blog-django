eb79c7db70f96c7e9b8751f92bd16b65a5b9b028
Renato Belini <renatobelini@gmail.com>
2024-04-25 10:53:39 -0300

    comitando

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,6 +1,8 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
-from blog.models import Post
+from blog.models import Post,Page
+from django.db.models import Q
+
 
 
 PER_PAGE = 9
@@ -61,6 +63,7 @@ def category(request, slug):
         }
     )
 
+
 def tag(request, slug):
     posts = Post.objects.get_published()\
         .filter(tags__slug=slug)
@@ -77,15 +80,43 @@ def tag(request, slug):
         }
     )
 
+def search(request):
+    search_value = request.GET.get('search', '').strip()
+
+    posts = (
+        Post.objects.get_published()
+        .filter(
+            Q(title__icontains=search_value) |
+            Q(excerpt__icontains=search_value) |
+            Q(content__icontains=search_value)
+        )[:PER_PAGE]
+    )
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': posts,
+            'search_value': search_value,
+        }
+    )
 
 def page(request, slug):
+    page = (
+        Page.objects
+        .filter(is_published=True)
+        .filter(slug=slug)
+        .first()
+    )
+    
     return render(
         request,
         'blog/pages/page.html',
         {
-            # 'page_obj': page_obj,
+           'page': page,
         }
     )
+
 
 
 def post(request, slug):
